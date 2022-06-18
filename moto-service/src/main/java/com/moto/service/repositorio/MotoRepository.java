a8bf3862d82d8fa12bbdfd6107984ef3f1612ae1
rodjess2021 <rodrigo.valer@tecsup.edu.pe>
2022-06-18 18:18:03 -0500

    Rest template/feign client

---
+++ moto-service/src/main/java/com/moto/service/repositorio/MotoRepository.java
@@ -0,0 +1,11 @@
+package com.moto.service.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.moto.service.entidades.Moto;
+
+public interface MotoRepository extends JpaRepository<Moto, Integer>{
+	List<Moto> findByUsuarioId(int usuarioId);
+
+}
